d12221eb9dd28ad56dd4a9ae6826046333e13c5d
MALAWIA <167401436+MALAWIA@users.noreply.github.com>
2024-09-10 12:10:51 +0300

    Add files via upload

---
+++ CARS.sql
@@ -0,0 +1,14 @@
+create table cars 
+(brand VARCHAR (225))
+ model INT,
+ carNO INT,
+ primary key(carNO);
+
+ insert into cars(brand,model,carNO)
+ values("Ferrari,428 spider,001");
+
+ SELECT * from cars;
+ 
+
+
+
